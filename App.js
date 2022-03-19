811b5e2c86581e1e03bfec6afd91a1884ad2d218
Ayush-Navadiya <ayushnavadiya@gmail.com>
2022-03-19 15:33:04 +0530

    initial Commit

--- App.js
+++ App.js
@@ -4,17 +4,31 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* Upper View */}
+      <View style={styles.container}>
+        <Text>Stuff in view 1!</Text>
+      </View>
+      {/* Lower View */}
+      <View style={styles.container1}>
+        <Text style={{color: '#fff'}}>Stuff in view 2!</Text>
+      </View>
     </View>
   );
 }
 
+// container Styles
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
+    justifyContent: 'center',
+  },
+  container1: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    width: '100%',
     justifyContent: 'center',
   },
 });
